5cbc970ac71b44c15e36327a045df89e1a1c1230
thanapdev <thanap151255@gmail.com>
2023-02-25 22:26:24 +0700

    demo 4

--- js/main.js
+++ js/main.js
@@ -7,10 +7,27 @@
 // });
 
 // nav
-function toggleMenu(){
-	const toggleMenu = document.querySelector(".toggleMenu");
-	const navigation = document.querySelector(".navigation");
-	toggleMenu.classList.toggle("active");
-	navigation.classList.toggle("active");
-}
+	let menu = document.querySelector('#menu-bars');
+	let navbar = document.querySelector('.navbar');
+
+	menu.onclick = () =>{
+		menu.classList.toggle('fa-times');
+		navbar.classList.toggle('active');
+	}
 // end nav
+
+// hero
+var swiper = new Swiper(".home-slider", {
+	spaceBetween: 30,
+	centeredSlides: true,
+	autoplay: {
+	  delay: 5500,
+	  disableOnInteraction: false,
+	},
+	pagination: {
+	  el: ".swiper-pagination",
+	  clickable: true,
+	},
+	loop:true,
+  });
+// end hero
