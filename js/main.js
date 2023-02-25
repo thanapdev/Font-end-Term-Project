63d1d3acc4731d5bb2da586226b4650e069f5df5
Ratchaporn <hrkfan321@gmail.com>
2023-02-25 15:05:17 +0700

    add html and images

--- js/main.js
+++ js/main.js
@@ -14,3 +14,34 @@ function toggleMenu(){
 	navigation.classList.toggle("active");
 }
 // end nav
+var product = [{
+	id: 1,
+	img: 'hero3.png',
+	name: 'Chicken and Rice',
+	price: 50,
+	description: 'Chicken and Rice',
+	type: 'A la Carte'
+}, {
+	id: 2,
+	img: 'hero2.png',
+	name: 'Fried Chicken',
+	price: 50,
+	description: 'Fried Chicken',
+	type: 'A la Carte'
+}, {
+	id: 3,
+	img: 'hero4.png',
+	name: 'Twin Chicken',
+	price: 50,
+	description: 'Twin Chicken',
+	type: 'A la Carte'
+}];
+
+$(document).ready(() =>{
+	var html = '';
+	// i = 0
+	for (let i = 0; i < array.length; i++) {
+		
+		
+	}
+})
