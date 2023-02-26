d4ff41892685e95c1d0025b16a456edf8195478e
thanapdev <thanap151255@gmail.com>
2023-02-26 18:08:49 +0700

    demo 6 add how to order

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-// responsive navbar
-// let mainNav = document.getElementById('js-menu');
-// let navbarToggle = document.getElementById('js-nav-toggle');
-
-// navbarToggle.addEventListener("click" , function(){
-//     mainNav.classList.toggle('active');
-// });
-
 // nav
 	let menu = document.querySelector('#menu-bars');
 	let navbar = document.querySelector('.navbar');
