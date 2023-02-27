5040712ff512a53fefc9120fb3f440240790900b
Ratchaporn <hrkfan321@gmail.com>
2023-02-27 12:52:17 +0700

    add option selector in order

--- js/main.js
+++ js/main.js
@@ -7,27 +7,29 @@
 // });
 
 // nav
-	let menu = document.querySelector('#menu-bars');
-	let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
 
-	menu.onclick = () =>{
-		menu.classList.toggle('fa-times');
-		navbar.classList.toggle('active');
-	}
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 // end nav
 
 // hero
 var swiper = new Swiper(".home-slider", {
-	spaceBetween: 30,
-	centeredSlides: true,
-	autoplay: {
-	  delay: 5500,
-	  disableOnInteraction: false,
-	},
-	pagination: {
-	  el: ".swiper-pagination",
-	  clickable: true,
-	},
-	loop:true,
-  });
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+
 // end hero
